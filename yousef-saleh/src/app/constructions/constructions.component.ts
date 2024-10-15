c3f5813cb8b97dc944a3e5bc36a769e0d7603835
Adil Qureshi <adil.aq48@gmail.com>
2024-10-15 21:42:49 +0530

    some changes

---
+++ yousef-saleh/src/app/constructions/constructions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-constructions',
+  standalone: true,
+  imports: [],
+  templateUrl: './constructions.component.html',
+  styleUrl: './constructions.component.css'
+})
+export class ConstructionsComponent {
+
+}
