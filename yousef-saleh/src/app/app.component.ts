332f3dd05c260aeef2c5bfd19a6c7859bfee5c83
Adil Qureshi <adil.aq48@gmail.com>
2024-10-15 22:30:10 +0530

    construction section complete

--- yousef-saleh/src/app/app.component.ts
+++ yousef-saleh/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormComponent } from "./form/form.component";
 import { HeaderComponent } from './header/header.component';
+import { ConstructionsComponent } from "./constructions/constructions.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent, FormComponent],
+  imports: [RouterOutlet, HeaderComponent, FormComponent, ConstructionsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
