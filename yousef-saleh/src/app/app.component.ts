4826c0c34cec680375588badd995be3a6d3d5412
Adil Qureshi <adil.aq48@gmail.com>
2024-10-15 23:15:37 +0530

    complete the service component

--- yousef-saleh/src/app/app.component.ts
+++ yousef-saleh/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormComponent } from "./form/form.component";
 import { HeaderComponent } from './header/header.component';
+import { ServicesComponent } from './services/services.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent, FormComponent],
+  imports: [RouterOutlet,HeaderComponent, FormComponent,ServicesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
