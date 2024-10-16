e6a72cb10b167aa2749ca1b761cd3c0518bfe09c
Adil Qureshi <adil.aq48@gmail.com>
2024-10-17 01:13:42 +0530

    code dynamic

--- yousef-saleh/src/app/project/project.component.ts
+++ yousef-saleh/src/app/project/project.component.ts
@@ -1,12 +1,20 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './project.component.html',
   styleUrl: './project.component.css'
 })
 export class ProjectComponent {
-
+  images = [
+    "https://www.bootstrapmade.com/content/demo/UpConstruction/assets/img/projects/remodeling-1.jpg",
+    "https://www.bootstrapmade.com/content/demo/UpConstruction/assets/img/projects/construction-1.jpg",
+    "https://www.bootstrapmade.com/content/demo/UpConstruction/assets/img/projects/repairs-1.jpg",
+    "https://www.bootstrapmade.com/content/demo/UpConstruction/assets/img/projects/design-1.jpg",
+    "https://www.bootstrapmade.com/content/demo/UpConstruction/assets/img/projects/remodeling-2.jpg",
+    "https://www.bootstrapmade.com/content/demo/UpConstruction/assets/img/projects/construction-2.jpg"
+  ];
 }
