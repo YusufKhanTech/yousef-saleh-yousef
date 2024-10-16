25396b38b3fbe733a9b668dbb82c79890ac855f3
Adil Qureshi <adil.aq48@gmail.com>
2024-10-16 17:23:15 +0530

    add aos animation

--- yousef-saleh/src/app/app.component.ts
+++ yousef-saleh/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, RouterOutlet } from '@angular/router';
 import { FormComponent } from './form/form.component';
 import { HeaderComponent } from './header/header.component';
 import { ConstructionsComponent } from './constructions/constructions.component';
 import { ServicesComponent } from './services/services.component';
 import { ProjectComponent } from './project/project.component';
 import { FooterComponent } from './footer/footer.component';
-
+import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -24,4 +24,10 @@ import { FooterComponent } from './footer/footer.component';
 })
 export class AppComponent {
   title = 'yousef-saleh';
+  ngOnInit() {
+    AOS.init({
+      duration: 1000,  // Default animation duration in milliseconds
+      once: true,  // Animation occurs only once
+    });
+  }
 }
