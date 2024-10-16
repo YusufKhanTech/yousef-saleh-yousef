45b433ad3d7340201560eb9e9cd9f948532e05a1
Adil Qureshi <adil.aq48@gmail.com>
2024-10-17 01:52:34 +0530

    code formatted

--- yousef-saleh/src/app/project/project.component.ts
+++ yousef-saleh/src/app/project/project.component.ts
@@ -6,16 +6,17 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [NgFor],
   templateUrl: './project.component.html',
-  styleUrl: './project.component.css'
+  styleUrl: './project.component.css',
 })
 export class ProjectComponent {
-  projectTitleName = "Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit";
+  projectTitleName =
+    'Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit';
   projectImages = [
-    "https://www.bootstrapmade.com/content/demo/UpConstruction/assets/img/projects/remodeling-1.jpg",
-    "https://www.bootstrapmade.com/content/demo/UpConstruction/assets/img/projects/construction-1.jpg",
-    "https://www.bootstrapmade.com/content/demo/UpConstruction/assets/img/projects/repairs-1.jpg",
-    "https://www.bootstrapmade.com/content/demo/UpConstruction/assets/img/projects/design-1.jpg",
-    "https://www.bootstrapmade.com/content/demo/UpConstruction/assets/img/projects/remodeling-2.jpg",
-    "https://www.bootstrapmade.com/content/demo/UpConstruction/assets/img/projects/construction-2.jpg"
+    'https://www.bootstrapmade.com/content/demo/UpConstruction/assets/img/projects/remodeling-1.jpg',
+    'https://www.bootstrapmade.com/content/demo/UpConstruction/assets/img/projects/construction-1.jpg',
+    'https://www.bootstrapmade.com/content/demo/UpConstruction/assets/img/projects/repairs-1.jpg',
+    'https://www.bootstrapmade.com/content/demo/UpConstruction/assets/img/projects/design-1.jpg',
+    'https://www.bootstrapmade.com/content/demo/UpConstruction/assets/img/projects/remodeling-2.jpg',
+    'https://www.bootstrapmade.com/content/demo/UpConstruction/assets/img/projects/construction-2.jpg',
   ];
 }
