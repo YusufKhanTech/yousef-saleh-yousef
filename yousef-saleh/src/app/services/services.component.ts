96200eebaa762d518e3a9355e1c94647690630d0
Adil Qureshi <adil.aq48@gmail.com>
2024-10-18 02:23:24 +0530

    complete services tab

--- yousef-saleh/src/app/services/services.component.ts
+++ yousef-saleh/src/app/services/services.component.ts
@@ -1,14 +1,16 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { BannerContentComponent } from '../banner-content/banner-content.component';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [BannerContentComponent],
   templateUrl: './services.component.html',
   styleUrl: './services.component.css',
 })
 export class ServicesComponent {
-  @Input() serviceDetail = false;
+  isServicesBanner: boolean = false;
   serviceCardData = [
     {
       heading: 'Quasi eaque omnis',
@@ -51,4 +53,8 @@ export class ServicesComponent {
         'Eius non minus autem soluta ut ui labore omnis quisquam corrupti autemodit voluptas quos commodi magnam occaecati.',
     },
   ];
+
+  constructor(private router: Router) {
+    this.isServicesBanner = this.router.url.includes('services');
+  }
 }
