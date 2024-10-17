a0e1c3d37d7b50e3bc5b63144674133a1e0e5ec9
Adil Qureshi <adil.aq48@gmail.com>
2024-10-18 02:10:32 +0530

    changes in services

--- yousef-saleh/src/app/app.routes.ts
+++ yousef-saleh/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent, // Home page or project list
+  },
+  {
+    path:'services',
+    component:ServicesComponent
   },
   {
     path: 'project',
--- yousef-saleh/src/app/services/services.component.ts
+++ yousef-saleh/src/app/services/services.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-services',
   standalone: true,
   imports: [],
   templateUrl: './services.component.html',
-  styleUrl: './services.component.css'
+  styleUrl: './services.component.css',
 })
 export class ServicesComponent {
-
+  @Input() serviceDetail = false;
+  serviceCardData = [
+    {
+      heading: 'Quasi eaque omnis',
+      subHeading:
+        'Eius non minus autem soluta ut ui labore omnis quisquam corrupti autemodit voluptas quos commodi magnam occaecati.',
+    },
+    {
+      heading: 'Quasi eaque omnis',
+      subHeading:
+        'Eius non minus autem soluta ut ui labore omnis quisquam corrupti autemodit voluptas quos commodi magnam occaecati.',
+    },
+    {
+      heading: 'Quasi eaque omnis',
+      subHeading:
+        'Eius non minus autem soluta ut ui labore omnis quisquam corrupti autemodit voluptas quos commodi magnam occaecati.',
+    },
+    {
+      heading: 'Quasi eaque omnis',
+      subHeading:
+        'Eius non minus autem soluta ut ui labore omnis quisquam corrupti autemodit voluptas quos commodi magnam occaecati.',
+    },
+    {
+      heading: 'Quasi eaque omnis',
+      subHeading:
+        'Eius non minus autem soluta ut ui labore omnis quisquam corrupti autemodit voluptas quos commodi magnam occaecati.',
+    },
+    {
+      heading: 'Quasi eaque omnis',
+      subHeading:
+        'Eius non minus autem soluta ut ui labore omnis quisquam corrupti autemodit voluptas quos commodi magnam occaecati.',
+    },
+    {
+      heading: 'Quasi eaque omnis',
+      subHeading:
+        'Eius non minus autem soluta ut ui labore omnis quisquam corrupti autemodit voluptas quos commodi magnam occaecati.',
+    },
+    {
+      heading: 'Quasi eaque omnis',
+      subHeading:
+        'Eius non minus autem soluta ut ui labore omnis quisquam corrupti autemodit voluptas quos commodi magnam occaecati.',
+    },
+  ];
 }
