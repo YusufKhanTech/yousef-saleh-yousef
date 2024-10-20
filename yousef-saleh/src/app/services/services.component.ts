60de1f2bb45fa53ed840065a765cd8313a11ee9a
Adil Qureshi <adil.aq48@gmail.com>
2024-10-20 17:34:26 +0530

    add routing import

--- yousef-saleh/src/app/services/services.component.ts
+++ yousef-saleh/src/app/services/services.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HeadingContentComponent } from "../heading-content/heading-content.component";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-services',
