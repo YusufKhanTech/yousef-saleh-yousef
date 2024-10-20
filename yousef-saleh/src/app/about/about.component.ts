0df7fb00cbc9cdde1186feceb1c36c4a0d2a97d8
Adil Qureshi <adil.aq48@gmail.com>
2024-10-20 13:28:00 +0530

    about container changes

--- yousef-saleh/src/app/about/about.component.ts
+++ yousef-saleh/src/app/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BannerContentComponent } from "../banner-content/banner-content.component";
+import { HeadingContentComponent } from "../heading-content/heading-content.component";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [BannerContentComponent],
+  imports: [BannerContentComponent, HeadingContentComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
