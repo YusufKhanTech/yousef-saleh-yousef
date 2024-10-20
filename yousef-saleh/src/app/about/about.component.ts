4c0f32195abd505053503412672fb6ea2fcd5d9c
Adil Qureshi <adil.aq48@gmail.com>
2024-10-20 16:20:17 +0530

    about container add and responsive

--- yousef-saleh/src/app/about/about.component.ts
+++ yousef-saleh/src/app/about/about.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
-import { BannerContentComponent } from "../banner-content/banner-content.component";
-import { HeadingContentComponent } from "../heading-content/heading-content.component";
+import { BannerContentComponent } from '../banner-content/banner-content.component';
+import { HeadingContentComponent } from '../heading-content/heading-content.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [BannerContentComponent, HeadingContentComponent],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
 export class AboutComponent {
-
+  cardContent = [
+    {
+      title: '232',
+      subTitle: 'Happy Clients',
+    },
+    {
+      title: '521',
+      subTitle: 'Projects',
+    },
+    {
+      title: '1463',
+      subTitle: 'Hours of Support',
+    },
+    {
+      title: '15',
+      subTitle: 'Hard Workers',
+    },
+  ];
 }
