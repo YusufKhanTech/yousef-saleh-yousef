8d635b4ff231764919fd06a570d8e24e5d9bf372
Adil Qureshi <adil.aq48@gmail.com>
2024-10-21 17:37:02 +0530

    add routing

--- yousef-saleh/src/app/app.routes.ts
+++ yousef-saleh/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ServicesComponent } from './services/services.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 export const routes: Routes = [
   {
@@ -11,6 +12,10 @@ export const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactUsComponent,
   },
   {
     path:'services',
--- yousef-saleh/src/app/contact-us/contact-us.component.ts
+++ yousef-saleh/src/app/contact-us/contact-us.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BannerContentComponent } from "../banner-content/banner-content.component";
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [],
+  imports: [BannerContentComponent],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.css'
 })
