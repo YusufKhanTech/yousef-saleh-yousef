688526ca76396c8d47b0086f3f7599527d9f18ac
yusuf.khan.ilmtec <yusuf.khan@ilmtec.in>
2024-10-22 18:08:09 +0300

    optimised code

--- yousef-saleh/src/app/home/home.component.ts
+++ yousef-saleh/src/app/home/home.component.ts
@@ -22,4 +22,54 @@ import { HeadingContentComponent } from "../heading-content/heading-content.comp
 })
 export class HomeComponent {
 
+  clientImages = [
+    {
+      clientName: '',
+      logo: '/assets/img/clients/ascott-logo.png'
+    },
+    {
+      clientName: '',
+      logo: '/assets/img/clients/bank-al-bilad-logo.png'
+    },
+    {
+      clientName: '',
+      logo: '/assets/img/clients/anb-bank-logo.png'
+    },
+    {
+      clientName: '',
+      logo: '/assets/img/clients/altoukhi-logo.jpg'
+    },
+    {
+      clientName: '',
+      logo: '/assets/img/clients/rcjy-logo.png'
+    },
+    {
+      clientName: '',
+      logo: '/assets/img/clients/ncr-logo.png'
+    },
+    {
+      clientName: '',
+      logo: '/assets/img/clients/al-rajhi-bank-logo.png'
+    },
+    {
+      clientName: '',
+      logo: '/assets/img/clients/sab-bank-logo.png'
+    },
+    {
+      clientName: '',
+      logo: '/assets/img/clients/riyad-logo.png'
+    },
+    {
+      clientName: '',
+      logo: '/assets/img/clients/national-grid-logo.png'
+    },
+    {
+      clientName: '',
+      logo: '/assets/img/clients/saudi-electricity-logo.png'
+    },
+    {
+      clientName: '',
+      logo: '/assets/img/clients/al-awwal-bank-logo.png'
+    },
+  ]
 }
