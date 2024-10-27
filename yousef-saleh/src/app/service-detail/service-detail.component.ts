c971399274e1638ddbf7c28e827890b58f388c10
yusuf.khan.ilmtec <yusuf.khan@ilmtec.in>
2024-10-27 18:16:04 +0300

    updated content as dynamic

--- yousef-saleh/src/app/service-detail/service-detail.component.ts
+++ yousef-saleh/src/app/service-detail/service-detail.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {BannerContentComponent} from '../banner-content/banner-content.component';
+import {ActivatedRoute} from '@angular/router';
+import {ServiceIncludes, ServiceKeyPoints, ServiceModel} from '../services/model/service.model';
+import {ServicesUtil} from '../services/util/service.util';
+import {JsonPipe} from '@angular/common';
 
 @Component({
   selector: 'app-service-detail',
@@ -10,12 +14,28 @@ import {BannerContentComponent} from '../banner-content/banner-content.component
   templateUrl: './service-detail.component.html',
   styleUrl: './service-detail.component.css'
 })
-export class ServiceDetailComponent {
+export class ServiceDetailComponent implements OnInit {
 
-  selectedService: string = 'web-design';
+  serviceId?: number;
+  service?: ServiceModel;
+  selectedService?: string;
+
+  constructor(private activatedRoute: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.getServiceId();
+  }
 
   selectService(event: Event, service: string): void {
     event.preventDefault();
     this.selectedService = service;
+  }
+
+  private getServiceId(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.serviceId = +params['serviceId'];
+      this.service = ServicesUtil.getServices().find(s => s.serviceId === this.serviceId);
+    });
   }
 }
