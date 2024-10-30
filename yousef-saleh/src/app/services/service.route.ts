7b64c5563dd6855290fea3bd1c190e089241c2f2
yusuf.khan.ilmtec <yusuf.khan@ilmtec.in>
2024-10-30 15:56:37 +0300

    updated routes

--- yousef-saleh/src/app/services/service.route.ts
+++ yousef-saleh/src/app/services/service.route.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import {ServicesComponent} from './services.component';
 import {ServiceDetailComponent} from '../service-detail/service-detail.component';
+import {ServiceCategoryComponent} from '../service-category/service-category.component';
 
 export const serviceRoutes: Routes = [
   {
     path: '',
     component: ServicesComponent
+  },
+  {
+    path: 'service-category',
+    component: ServiceCategoryComponent
   },
   {
     path: ':serviceId',
