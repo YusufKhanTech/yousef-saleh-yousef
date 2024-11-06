335d6af4874ec90d30afc54308895f70d5ec1f27
Adil Qureshi <adil.aq48@gmail.com>
2024-11-06 17:56:36 +0530

    changes

--- yousef-saleh/src/app/home/home.component.ts
+++ yousef-saleh/src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import {ProjectComponent} from '../project/project.component';
 import {ServicesComponent} from '../services/services.component';
 import {BannerContentComponent} from '../banner-content/banner-content.component';
 import {ClientComponent} from '../client/client.component';
+import { HeadingContentComponent } from "../heading-content/heading-content.component";
 
 @Component({
   selector: 'app-home',
@@ -11,8 +12,9 @@ import {ClientComponent} from '../client/client.component';
     BannerContentComponent,
     ServicesComponent,
     ProjectComponent,
-    ClientComponent
-  ],
+    ClientComponent,
+    HeadingContentComponent
+],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
