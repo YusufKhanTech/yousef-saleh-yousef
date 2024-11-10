98a193406c71fa3bb46ecf3789450bd06bf2f839
yusuf.khan.ilmtec <yusuf.khan@ilmtec.in>
2024-11-10 23:14:31 +0300

    fixed scroll issue

--- yousef-saleh/src/app/app.component.ts
+++ yousef-saleh/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
 import * as AOS from 'aos';
 import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
@@ -16,12 +16,29 @@ import {FooterComponent} from './footer/footer.component';
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
+
   title = 'yousef-saleh';
 
+  constructor(private router: Router) {
+  }
+
   ngOnInit() {
+    this.initializeAOS();
+    this.scrollToTopOfPage();
+  }
+
+  private initializeAOS(): void {
     AOS.init({
       duration: 1000,
       once: true,
+    });
+  }
+
+  private scrollToTopOfPage(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0);
+      }
     });
   }
 }
