cdaeaecb5c3930da42b98b7243fdc1668aaea9aa
Adil Qureshi <adil.aq48@gmail.com>
2024-11-10 18:51:29 +0530

    our projects complete tab

--- yousef-saleh/src/app/our-projects/our-projects.component.ts
+++ yousef-saleh/src/app/our-projects/our-projects.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { BannerContentComponent } from '../banner-content/banner-content.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-our-projects',
   standalone: true,
-  imports: [],
+  imports: [BannerContentComponent,NgClass],
   templateUrl: './our-projects.component.html',
   styleUrl: './our-projects.component.css'
 })
 export class OurProjectsComponent {
+tabs = ["All","Constrctions","Water Proffing","Maintenance and operations"];
+activeTabIndex = 0;
 
+selectedTab(index:number){
+  this.activeTabIndex = index;
+}
 }
