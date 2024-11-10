b2a4d440bb3c91d1d14fc991f1e674eb8bee1475
Adil Qureshi <adil.aq48@gmail.com>
2024-11-10 19:06:29 +0530

    create a card for projects

--- yousef-saleh/src/app/our-projects/our-projects.component.ts
+++ yousef-saleh/src/app/our-projects/our-projects.component.ts
@@ -1,19 +1,31 @@
 import { Component } from '@angular/core';
 import { BannerContentComponent } from '../banner-content/banner-content.component';
 import { NgClass } from '@angular/common';
+import { ProjectComponent } from '../project/project.component';
 
 @Component({
   selector: 'app-our-projects',
   standalone: true,
-  imports: [BannerContentComponent,NgClass],
+  imports: [BannerContentComponent, NgClass, ProjectComponent],
   templateUrl: './our-projects.component.html',
-  styleUrl: './our-projects.component.css'
+  styleUrl: './our-projects.component.css',
 })
 export class OurProjectsComponent {
-tabs = ["All","Constrctions","Water Proffing","Maintenance and operations"];
-activeTabIndex = 0;
+  tabs = [
+    'All',
+    'Constrctions',
+    'Water Proffing',
+    'Maintenance and operations',
+  ];
+  projectsImage = [
+    'https://html.rrdevs.net/ribuild/assets/imgs/our-projects/latest-project__item-1.jpg',
+    'https://html.rrdevs.net/ribuild/assets/imgs/our-projects/latest-project__item-2.jpg',
+    'https://html.rrdevs.net/ribuild/assets/imgs/our-projects/latest-project__item-1.jpg',
+    'https://html.rrdevs.net/ribuild/assets/imgs/our-projects/latest-project__item-2.jpg',
+  ];
+  activeTabIndex = 0;
 
-selectedTab(index:number){
-  this.activeTabIndex = index;
-}
+  selectedTab(index: number) {
+    this.activeTabIndex = index;
+  }
 }
