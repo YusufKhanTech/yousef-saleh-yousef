88f60d2b28347dde03d8b54c815b37957301506e
Adil Qureshi <adil.aq48@gmail.com>
2024-11-10 18:52:05 +0530

    add our projects components

--- yousef-saleh/src/app/home/home.component.ts
+++ yousef-saleh/src/app/home/home.component.ts
@@ -1,22 +1,23 @@
-import {Component} from '@angular/core';
-import {ProjectComponent} from '../project/project.component';
-import {ServicesComponent} from '../services/services.component';
-import {BannerContentComponent} from '../banner-content/banner-content.component';
-import {ClientComponent} from '../client/client.component';
-import { HeadingContentComponent } from "../heading-content/heading-content.component";
+import { Component } from '@angular/core';
+import { ProjectComponent } from '../project/project.component';
+import { ServicesComponent } from '../services/services.component';
+import { BannerContentComponent } from '../banner-content/banner-content.component';
+import { ClientComponent } from '../client/client.component';
+import { HeadingContentComponent } from '../heading-content/heading-content.component';
+import { OurProjectsComponent } from '../our-projects/our-projects.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     BannerContentComponent,
+    OurProjectsComponent,
     ServicesComponent,
     ProjectComponent,
     ClientComponent,
-    HeadingContentComponent
-],
+    HeadingContentComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-}
+export class HomeComponent {}
