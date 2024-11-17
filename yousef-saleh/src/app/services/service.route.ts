fbb0182cab09df05c8618d1c454ad7063df454e2
yusuf.khan.ilmtec <yusuf.khan@ilmtec.in>
2024-11-17 15:36:57 +0300

    removed unused imports

--- yousef-saleh/src/app/services/service.route.ts
+++ yousef-saleh/src/app/services/service.route.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import {ServicesComponent} from './services.component';
-import {ServiceDetailComponent} from '../service-detail/service-detail.component';
 import {ServiceCategoryComponent} from '../service-category/service-category.component';
 
 export const serviceRoutes: Routes = [
