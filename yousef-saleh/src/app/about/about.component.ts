01f4c270e4e549acf2099d68aa8d534a24ba75b2
yusuf.khan.ilmtec <yusuf.khan@ilmtec.in>
2024-11-19 20:26:51 +0300

    used util class to get data for employees

--- yousef-saleh/src/app/about/about.component.ts
+++ yousef-saleh/src/app/about/about.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { BannerContentComponent } from '../banner-content/banner-content.component';
 import { HeadingContentComponent } from '../heading-content/heading-content.component';
+import {EmployeeUtil} from './util/employee.util';
+import {NgTemplateOutlet} from '@angular/common';
+import {EmployeeAlignment} from './model/employee.model';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [BannerContentComponent, HeadingContentComponent],
+  imports: [BannerContentComponent, HeadingContentComponent, NgTemplateOutlet],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
@@ -13,81 +16,7 @@ import { HeadingContentComponent } from '../heading-content/heading-content.comp
 export class AboutComponent {
 
   breadcrumbTexts= ['Home', 'About Us'];
-  teamContent = [
-    // {
-    //   name: 'Afifi Farag',
-    //   designation: 'Financial Advisor',
-    //   image: 'assets/img/employees/afifi.jpeg'
-    // },
-    // {
-    //   name: 'Saqib Naseer',
-    //   designation: 'Document Controller',
-    //   image: 'assets/img/employees/saqib.jpeg'
-    // },
-    {
-      name: 'Mohammed Samid bashir Ulde',
-      designation: 'AutoCAD Draftsman & Supervisor',
-      image: 'assets/img/employees/samid.jpeg'
-    },
-    // {
-    //   name: 'Emad Mohamed Murtada',
-    //   designation: 'HR',
-    //   image: 'assets/img/employees/emad.jpeg'
-    // },
-    {
-      name: 'Mohammad Arif Khan',
-      designation: 'Project Engineer',
-      image: 'assets/img/employees/arif.jpeg'
-    },
-    {
-      name: 'Serajuddin Khan',
-      designation: 'Quantity Surveyor & Site Engineer',
-      image: 'assets/img/employees/serajuddin.jpeg'
-    },
-    {
-      name: 'Mohd Kaleem ',
-      designation: 'Site Engineer',
-      image: 'assets/img/employees/kaleem.jpeg'
-    },
-    {
-      name: 'Ali Tahir',
-      designation: 'Site Engineer',
-      image: 'assets/img/employees/ali.jpeg'
-    },
-    {
-      name: 'Rethal Arabi',
-      designation: 'Site Engineer',
-      image: 'assets/img/employees/rethal.jpeg'
-    },
-    {
-      name: 'Mohamed Yoosuf Asan Kani',
-      designation: 'Site Engineer',
-      image: 'assets/img/employees/yoosuf.jpeg'
-    },
-    {
-      name: 'Vidhadhar Jangio',
-      designation: 'Site Engineer',
-      image: 'assets/img/employees/vidhadhar.jpeg'
-    },
-    {
-      name: 'Mohammad Mustafa Jamil Ahmad',
-      designation: 'Site Engineer',
-      image: 'assets/img/employees/mustafa.jpeg'
-    },
-    {
-      name: 'Ayum Kabari Sabila Khatun',
-      designation: 'Site Engineer',
-      image: 'assets/img/employees/ayyum.jpeg'
-    },
-    {
-      name: 'Muhammad Asif Muhammad Arshad',
-      designation: 'Site Engineer',
-      image: 'assets/img/employees/asif.jpeg'
-    },
-    {
-      name: 'Mohammad Irfan Alam',
-      designation: 'Safety Supervisor',
-      image: 'assets/img/employees/irfan.jpeg'
-    },
-  ];
+  employeeContents = EmployeeUtil.getAllEmployees();
+  teamContent = EmployeeUtil.getAllTeamMembers();
+  protected readonly EmployeeAlignment = EmployeeAlignment;
 }
