ac0265da93409bb9291ed6bedf99356ca7b3ac04
yusuf.khan.ilmtec <yusuf.khan@ilmtec.in>
2024-12-04 13:26:11 +0300

    added routes for award

--- yousef-saleh/src/app/app.routes.ts
+++ yousef-saleh/src/app/app.routes.ts
@@ -1,17 +1,21 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { ServicesComponent } from './services/services.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import {AwardComponent} from './award/award.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent, // Home page or project list
+    component: HomeComponent,
   },
   {
     path: 'about',
     component: AboutComponent,
+  },
+  {
+    path: 'award',
+    component: AwardComponent,
   },
   {
     path: 'contact',
