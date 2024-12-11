45a2305dc123be13d2d6fb25744c30eb2b7e37e4
yusuf.khan.ilmtec <yusuf.khan@ilmtec.in>
2024-12-11 19:17:33 +0300

    fixed carousel issue

--- yousef-saleh/src/app/award/util/award-and-certificate.util.ts
+++ yousef-saleh/src/app/award/util/award-and-certificate.util.ts
@@ -3,13 +3,16 @@ import {AwardAndCertificate, CERTIFICATE_TYPE} from '../model/award-and-certific
 export class AwardAndCertificateUtil {
 
   public static commercialCertificates: AwardAndCertificate[] = [
-    { certificateId: 'cc01', certificateName: 'rcjy-logo', image: '/assets/img/awards/cc-1.jpeg' },
-    { certificateId: 'cc02', certificateName: 'chamber', image: '/assets/img/awards/cc-chamber.jpeg' },
-    { certificateId: 'cc03', certificateName: 'monshaat', image: '/assets/img/awards/cc-monshaat.jpeg' },
-    { certificateId: 'cc04', certificateName: 'registeration', image: '/assets/img/awards/cc-registeration.jpeg' },
-    { certificateId: 'cc05', certificateName: 'vat', image: '/assets/img/awards/cc-vat.jpeg' },
-    { certificateId: 'cc06', certificateName: 'zakat-1', image: '/assets/img/awards/cc-zakat-1.jpeg' },
-    { certificateId: 'cc06', certificateName: 'zakat', image: '/assets/img/awards/cc-zakat.jpeg' },
+    { certificateId: 'cc01', certificateName: 'registeration', image: '/assets/img/awards/cc-registeration.jpeg' },
+    { certificateId: 'cc02', certificateName: 'rcjy-logo', image: '/assets/img/awards/cc-1.jpeg' },
+    { certificateId: 'cc03', certificateName: 'chamber', image: '/assets/img/awards/cc-chamber.jpeg' },
+    { certificateId: 'cc04', certificateName: 'monshaat', image: '/assets/img/awards/cc-monshaat.jpeg' }
+  ];
+
+  public static aFourCommercialCertificates: AwardAndCertificate[] = [
+    { certificateId: 'acc01', certificateName: 'vat', image: '/assets/img/awards/cc-vat.jpeg' },
+    { certificateId: 'acc02', certificateName: 'zakat-1', image: '/assets/img/awards/cc-zakat-1.jpeg' },
+    { certificateId: 'acc03', certificateName: 'zakat', image: '/assets/img/awards/cc-zakat.jpeg' },
   ];
 
   public static preQualifiedCertificates: AwardAndCertificate[] = [
@@ -24,23 +27,30 @@ export class AwardAndCertificateUtil {
   ];
 
   public static  recognitionCertificates: AwardAndCertificate[] = [
-    { certificateId: 'rc01', certificateName: 'awards-saudi-electricity', image: '/assets/img/awards/awards-saudi-electricity.jpeg' },
-    { certificateId: 'rc03', certificateName: 'national-grid', image: '/assets/img/awards/COA-national-grid.jpeg' },
-    { certificateId: 'rc04', certificateName: 'civil-defense', image: '/assets/img/awards/COA-civil-defense.jpeg' },
-    { certificateId: 'rc05', certificateName: 'nc', image: '/assets/img/awards/COA-nc.jpeg' },
-    { certificateId: 'rc06', certificateName: 'zamil', image: '/assets/img/awards/COA-zamil.jpeg' },
+    { certificateId: 'rc01', certificateName: 'civil-defense', image: '/assets/img/awards/COA-civil-defense.jpeg' },
+    { certificateId: 'rc02', certificateName: 'nc', image: '/assets/img/awards/COA-nc.jpeg' },
+  ];
+
+  public static  aFourRecognitionCertificates: AwardAndCertificate[] = [
+    { certificateId: 'arc01', certificateName: 'awards-saudi-electricity', image: '/assets/img/awards/awards-saudi-electricity.jpeg' },
+    { certificateId: 'arc02', certificateName: 'national-grid', image: '/assets/img/awards/COA-national-grid.jpeg' },
+    { certificateId: 'arc03', certificateName: 'zamil', image: '/assets/img/awards/COA-zamil.jpeg' },
   ];
 
   public static getCertificatesByCategory(category: string): AwardAndCertificate[] {
     switch (category) {
       case CERTIFICATE_TYPE.COMMERCIAL:
         return this.commercialCertificates;
+      case CERTIFICATE_TYPE.A_FOUR_COMMERCIAL:
+        return this.aFourCommercialCertificates;
       case CERTIFICATE_TYPE.PREQUALIFIED:
         return this.preQualifiedCertificates;
       case CERTIFICATE_TYPE.ISO:
         return this.isoCertificates;
       case CERTIFICATE_TYPE.RECOGNITION:
         return this.recognitionCertificates;
+      case CERTIFICATE_TYPE.A_FOUR_RECOGNITION:
+        return this.aFourRecognitionCertificates;
       default:
         return [];
     }
