f69e7f328efc7cc2d9ae225b933bc22cd4bba7d7
yusuf.khan.ilmtec <yusuf.khan@ilmtec.in>
2024-12-15 13:35:12 +0300

    used util and removed unused code

--- yousef-saleh/src/app/project/project.component.ts
+++ yousef-saleh/src/app/project/project.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { HeadingContentComponent } from '../heading-content/heading-content.component';
+import {ProjectUtil} from './util/project.util';
 
 @Component({
   selector: 'app-project',
@@ -10,40 +11,12 @@ import { HeadingContentComponent } from '../heading-content/heading-content.comp
   styleUrl: './project.component.css',
 })
 export class ProjectComponent {
-  projects = [
-    {
-      id: 1,
-      name: 'Riyad Bank',
-      imageUrl: '/assets/img/website-images/riyad-bank-project-1.jpg',
-    },
-    {
-      id: 2,
-      name: 'Saudi Electricity Company',
-      imageUrl: '/assets/img/website-images/substation-1.jpg',
-    },
-    {
-      id: 3,
-      name: 'Al Rajhi Bank',
-      imageUrl: '/assets/img/website-images/al-rajhi-project-3.jpg',
-    },
-    {
-      id: 4,
-      name: 'Al Bilad Bank',
-      imageUrl: '/assets/img/website-images/al-bilad-project-4.jpg',
-    },
-    {
-      id: 5,
-      name: 'Saudi Investment Bank',
-      imageUrl: '/assets/img/website-images/saudi-investment-bank.jpg',
-    },
-    {
-      id: 5,
-      name: 'Saudi Electricity Company',
-      imageUrl: '/assets/img/website-images/substation-2.jpg',
-    },
-  ];
-  constructor(private router: Router) {}
-  navigateToProjectDetail(projectId: number) {
+  projects = ProjectUtil.getAllProjects().filter(project => project?.isFeaturedProject);
+
+  constructor(private router: Router) {
+  }
+
+  navigateToProjectDetail(projectId?: number) {
     projectId
       ? this.router.navigate(['project/', projectId])
       : Error('undefined id');
