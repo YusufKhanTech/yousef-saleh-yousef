34a8cb0a5889621d689e2195ce175373e4f4b731
yusuf.khan.ilmtec <yusuf.khan@ilmtec.in>
2025-01-08 12:03:37 +0300

    removed unused code and fixed include content issue

--- yousef-saleh/src/app/service-category/service-category.component.ts
+++ yousef-saleh/src/app/service-category/service-category.component.ts
@@ -2,20 +2,21 @@ import {Component, OnInit} from '@angular/core';
 import {BannerContentComponent} from '../banner-content/banner-content.component';
 import {ServiceModel} from '../services/model/service.model';
 import {ServicesUtil} from '../services/util/service.util';
-import {ActivatedRoute, RouterOutlet} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-service-category',
   standalone: true,
   imports: [
     BannerContentComponent
-],
+  ],
   templateUrl: './service-category.component.html',
   styleUrl: './service-category.component.css'
 })
 export class ServiceCategoryComponent implements OnInit {
 
   selectedService?: ServiceModel;
+  selectedServiceInclude?: string;
   serviceCategoryId?: number;
   allServices?: ServiceModel[] | undefined = [];
   breadcrumbTexts= ['Home', 'Service Category'];
@@ -33,13 +34,16 @@ export class ServiceCategoryComponent implements OnInit {
   selectService(event: Event, service: ServiceModel): void {
     event.preventDefault();
     this.selectedService = service;
-    console.log("hellow");
-    
+  }
+
+  selectServiceInclude(event: Event, serviceInclude: string | undefined): void {
+    event.preventDefault();
+    this.selectedServiceInclude = serviceInclude;
   }
 
   private setCategoryId(): void {
     this.activatedRoute.params.subscribe(params => {
-      this.serviceCategoryId = +params['serviceCategoryId']; // Convert serviceId to a number
+      this.serviceCategoryId = +params['serviceCategoryId'];
       this.allServices = ServicesUtil.getAllServices().find(s=> s?.serviceCategoryId === this.serviceCategoryId)?.serviceSubCategories;
     });
   }
