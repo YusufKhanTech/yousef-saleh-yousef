05ff0565025f3759b904626c0e879657003b8e37
yusuf.khan.ilmtec <yusuf.khan@ilmtec.in>
2025-03-12 14:35:51 +0300

    updated content for the award util class

--- yousef-saleh/src/app/award/util/award-and-certificate.util.ts
+++ yousef-saleh/src/app/award/util/award-and-certificate.util.ts
@@ -3,32 +3,33 @@ import {AwardAndCertificate, CERTIFICATE_TYPE} from '../model/award-and-certific
 export class AwardAndCertificateUtil {
 
   public static commercialCertificates: AwardAndCertificate[] = [
-    { certificateId: 'cc01', certificateName: 'registeration', image: '/assets/img/awards/cc-registeration.jpeg' },
-    { certificateId: 'cc02', certificateName: 'rcjy-logo', image: '/assets/img/awards/cc-1.jpeg' },
-    { certificateId: 'cc03', certificateName: 'chamber', image: '/assets/img/awards/cc-chamber.jpeg' }
+    { certificateId: 'cc01', certificateName: 'registeration', image: '/assets/img/awards/ministry-of-commerce.png', showImage: '/assets/img/awards/cc-registeration.jpeg' },
+    { certificateId: 'cc02', certificateName: 'rcjy-logo', image: '/assets/img/awards/cc-1.jpeg', showImage: '/assets/img/awards/cc-1.jpeg' },
+    { certificateId: 'cc03', certificateName: 'chamber', image: '/assets/img/awards/asharqia-chamber.svg', showImage: '/assets/img/awards/cc-chamber.jpeg' },
+    { certificateId: 'cc04', certificateName: 'national-address', image: '/assets/img/awards/national-address.svg', showImage: '/assets/img/awards/cc-chamber.jpeg' },
+    { certificateId: 'cc05', certificateName: 'gosi', image: '/assets/img/awards/gosi.svg', showImage: '/assets/img/awards/cc-chamber.jpeg' },
   ];
 
   public static aFourCommercialCertificates: AwardAndCertificate[] = [
-    { certificateId: 'acc01', certificateName: 'vat', image: '/assets/img/awards/cc-vat.jpeg' },
-    { certificateId: 'acc02', certificateName: 'zakat-1', image: '/assets/img/awards/cc-zakat-1.jpeg' },
-    { certificateId: 'acc03', certificateName: 'zakat', image: '/assets/img/awards/cc-zakat.jpeg' }
+    { certificateId: 'acc01', certificateName: 'vat', image: '/assets/img/awards/zakat.png', showImage: '/assets/img/awards/cc-vat.jpeg' },
+    { certificateId: 'acc02', certificateName: 'zakat-1', image: '/assets/img/awards/zakat.png' , showImage: '/assets/img/awards/cc-zakat-1.jpeg' }
   ];
 
   public static preQualifiedCertificates: AwardAndCertificate[] = [
-    { certificateId: 'pqc01', certificateName: 'saudi-electricity', image: '/assets/img/awards/pq-saudi-electricity.jpeg' },
-    { certificateId: 'pqc02', certificateName: 'saudi-electricity-1', image: '/assets/img/awards/pq-saudi-electricity-1.jpeg' }
+    { certificateId: 'pqc01', certificateName: 'saudi-electricity', image: '/assets/img/clients/saudi-electricity-logo.png', showImage: '/assets/img/awards/pq-saudi-electricity.jpeg' },
+    { certificateId: 'pqc02', certificateName: 'saudi-electricity-1', image: '/assets/img/clients/saudi-electricity-logo.png', showImage: '/assets/img/awards/pq-saudi-electricity-1.jpeg' }
   ];
 
   public static  isoCertificates: AwardAndCertificate[] = [
-    { certificateId: 'ic01', certificateName: '9001-2015', image: '/assets/img/awards/9001-2015.jpeg' },
-    { certificateId: 'ic02', certificateName: '14001-2015', image: '/assets/img/awards/14001-2015.jpeg' },
-    { certificateId: 'ic03', certificateName: '45001-2018', image: '/assets/img/awards/45001-2018.jpeg' }
+    { certificateId: 'ic01', certificateName: '9001-2015', image: '/assets/img/awards/ISO-9001-2015-logo.jpg', showImage: '/assets/img/awards/9001-2015.jpeg'},
+    { certificateId: 'ic02', certificateName: '14001-2015', image: '/assets/img/awards/ISO-14001-2015-logo.jpg', showImage: '/assets/img/awards/14001-2015.jpeg'},
+    { certificateId: 'ic03', certificateName: '45001-2018', image: '/assets/img/awards/ISO-45001-2018-logo.jpg', showImage: '/assets/img/awards/45001-2018.jpeg' }
   ];
 
   public static  aFourRecognitionCertificates: AwardAndCertificate[] = [
-    { certificateId: 'arc02', certificateName: 'national-grid', image: '/assets/img/awards/COA-national-grid.jpeg' },
-    { certificateId: 'arc03', certificateName: 'zamil', image: '/assets/img/awards/COA-zamil.jpeg' },
-    { certificateId: 'arc01', certificateName: 'awards-saudi-electricity', image: '/assets/img/awards/awards-saudi-electricity.jpeg' }
+    { certificateId: 'arc02', certificateName: 'national-grid', image: '/assets/img/clients/national-grid-logo.png', showImage: '/assets/img/awards/COA-national-grid.jpeg' },
+    { certificateId: 'arc03', certificateName: 'zamil', image: '/assets/img/clients/national-grid-logo.png',showImage: '/assets/img/awards/COA-zamil.jpeg' },
+    { certificateId: 'arc01', certificateName: 'awards-saudi-electricity',image: '/assets/img/clients/national-grid-logo.png', showImage: '/assets/img/awards/awards-saudi-electricity.jpeg' }
   ];
 
   public static getCertificatesByCategory(category: string): AwardAndCertificate[] {
